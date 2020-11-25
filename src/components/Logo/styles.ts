776569c8e72e8c6a0fd64a6cd22a65faba8c6513
Jonas <jonaslagoteixeira@gmail.com>
2020-11-24 21:22:23 -0300

    foi feito componente de logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,3 +1,61 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { LogoProps } from '.';
 
-export const Wrapper = styled.div``;
+const wrapperModifiers = {
+  normal: () => css`
+    ${LogoWrapper} {
+      width: 4.5rem;
+      height: 4.5rem;
+    }
+
+    ${LogoText} {
+      font-size: 3.2rem;
+      left: calc(4.5rem + 0.5rem);
+      bottom: -1.2rem;
+    }
+  `,
+
+  large: () => css`
+    ${LogoWrapper} {
+      width: 5.9rem;
+      height: 5.9rem;
+    }
+
+    ${LogoText} {
+      font-size: 4rem;
+      left: calc(5.9rem + 0.5rem);
+      bottom: -1.2rem;
+    }
+  `,
+
+  hideOnMobile: () => css`
+    ${LogoWrapper} {
+      width: 4.5rem;
+      height: 4.5rem;
+    }
+
+    ${LogoText} {
+      display: none;
+      pointer-events: none;
+    }
+  `,
+};
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, size, color, hiddenOnMobile }) => css`
+    display: flex;
+    align-items: center;
+    position: relative;
+    color: ${theme.colors[color!]};
+
+    ${!!size && wrapperModifiers[size]};
+
+    ${!!hiddenOnMobile && wrapperModifiers.hideOnMobile};
+  `}
+`;
+
+export const LogoWrapper = styled.div``;
+
+export const LogoText = styled.p`
+  position: absolute;
+`;
