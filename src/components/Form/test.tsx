d0c477fca5d14b56097d04de5d730324b1374e84
Jonas <jonaslagoteixeira@gmail.com>
2020-11-26 17:25:24 -0300

    foi feito testes do form

--- src/components/Form/test.tsx
+++ src/components/Form/test.tsx
@@ -1,14 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { AddBox } from '@styled-icons/material-outlined/';
 
 import Form from '.';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import inputs from './mocks';
 
 describe('<Form />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Form />);
+  it('should render the form', () => {
+    const { container } = renderWithTheme(
+      <Form
+        inputs={inputs}
+        buttonLabel="Cadastrar"
+        buttonIcon={<AddBox size={24} />}
+      />,
+    );
 
-    expect(screen.getByRole('heading', { name: /Form/i }))
-    .toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /cadastrar/i }),
+    ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.parentElement).toMatchSnapshot();
   });
 });
